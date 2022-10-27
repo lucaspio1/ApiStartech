f965d0c3a4e9ce3c1a0f1215dd48b88eda910d8f
lucaspio1 <lucaspioh@gmail.com>
2022-10-26 23:44:50 -0300

    iniciando projeto

---
+++ api-ola-mundo/src/main/java/com/codacum/apiolamundo/ApiOlaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.codacum.apiolamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOlaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiOlaMundoApplication.class, args);
+	}
+
+}
---
+++ api-ola-mundo/src/test/java/com/codacum/apiolamundo/ApiOlaMundoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codacum.apiolamundo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOlaMundoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
