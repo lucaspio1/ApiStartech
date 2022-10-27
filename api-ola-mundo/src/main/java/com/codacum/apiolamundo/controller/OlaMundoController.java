2d55274c30221299cfb7ccd0cf7881bf530d27e5
lucaspio1 <lucaspioh@gmail.com>
2022-10-26 23:59:24 -0300

    Criada rota get e chamado restcontroller

--- api-ola-mundo/src/main/java/com/codacum/apiolamundo/controller/OlaMundoController.java
+++ api-ola-mundo/src/main/java/com/codacum/apiolamundo/controller/OlaMundoController.java
@@ -1,5 +1,12 @@
 package com.codacum.apiolamundo.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 
 public class OlaMundoController {
-    
+    @GetMapping("/ola")
+
+    public String olamundo(){
+        return "Ola Mundo!! Se você está vendo isso é porque Funcionou!";
+    }
 }
