f636ef981c2a0c9b17233ec0d8ec3e7152ed47d1
lucaspio1 <lucaspioh@gmail.com>
2022-11-01 02:39:56 -0300

    criada rotas

--- api-ola-mundo/src/main/java/com/codacum/apiolamundo/controller/OlaMundoController.java
+++ api-ola-mundo/src/main/java/com/codacum/apiolamundo/controller/OlaMundoController.java
@@ -1,12 +1,40 @@
 package com.codacum.apiolamundo.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+import com.codacum.apiolamundo.entity.StartechEntity;
+import com.codacum.apiolamundo.service.StarTechservice;
+
+
+
 @RestController
-
 public class OlaMundoController {
-    @GetMapping("/home")
+    @Autowired
+    private StarTechservice starTechservice;
 
-    public String olamundo(){
+    @GetMapping("/home")
+        public String olamundo(){
         return "Ola Mundo!! Se você está vendo isso é porque Funcionou!";
     }
+
+    @GetMapping("/users")
+    public List<StartechEntity> getAllUsers(){
+        return starTechservice.getUsers();
+    }
+
+    @GetMapping("/user/{id}")
+    public StartechEntity getUserById(@PathVariable int id){
+        return starTechservice.getUserById(id);
+    }
+
+    @PostMapping("/addUser")
+    public StartechEntity addUser(@RequestBody StartechEntity startechEntity) {
+        return starTechservice.createUser(startechEntity);
+    }  
+
 }
