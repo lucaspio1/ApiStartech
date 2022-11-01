1d870e3949cc6c8c7555af0b89ff60795135f3ce
lucaspio1 <lucaspioh@gmail.com>
2022-11-01 02:52:00 -0300

    rota delete

--- api-ola-mundo/src/main/java/com/codacum/apiolamundo/controller/OlaMundoController.java
+++ api-ola-mundo/src/main/java/com/codacum/apiolamundo/controller/OlaMundoController.java
@@ -1,8 +1,10 @@
 package com.codacum.apiolamundo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
@@ -36,5 +38,15 @@ public class OlaMundoController {
     public StartechEntity addUser(@RequestBody StartechEntity startechEntity) {
         return starTechservice.createUser(startechEntity);
     }  
+
+    @DeleteMapping("/user/{id}")
+	public String deleteUser(@PathVariable int id) {
+		return starTechservice.deleteUserById(id);
+	}
+
+
+
+
+
 
 }
--- api-ola-mundo/src/main/java/com/codacum/apiolamundo/service/StarTechservice.java
+++ api-ola-mundo/src/main/java/com/codacum/apiolamundo/service/StarTechservice.java
@@ -2,6 +2,7 @@ package com.codacum.apiolamundo.service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,6 +31,9 @@ public class StarTechservice {
     public StartechEntity getUserById(int id){
         return startechRepository.findById(id).orElse(null);
     }
-    
 
+   public String deleteUserById (int id){
+    startechRepository.deleteById(id);
+    return "Usuario deletado";
+   }
 }
